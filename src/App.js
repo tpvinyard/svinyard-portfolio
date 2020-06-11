bb432c21e96797b1224518c5a530da5dcacb5291
Tommy Vinyard <tpvinyard@gmail.com>
2020-06-10 19:38:33 -0500

    adding a bit of formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleContainer from './components/mainpage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SimpleContainer/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sarah Vinyard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Marketing Analyst from Austin, TX.
+        </p>
       </header>
     </div>
   );
